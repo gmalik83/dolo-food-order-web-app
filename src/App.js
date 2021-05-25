21ce0e1d5e45874187c03884d40cf8985b846b40
gmalik83 <g8383malik@gmail.com>
2021-05-26 00:19:58 +0530

    All done before using state

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {Fragment} from 'react';
+import Meals from './components/Meals/Meals';
+import Header from './components/Layout/Header';
+import Cart from './components/Cart/Cart';
+
+function App() {
+  return (
+    <Fragment>
+      <Cart/>
+      <Header/>
+      <main>
+        <Meals/>
+      </main>
+    </Fragment>
+    
+  );
+}
+
+export default App;
