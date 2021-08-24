183baf70e50db95df3d5f68e104875ffdd3b5a2a
gmalik83 <gaurav98malik@gmail.com>
2021-08-25 01:30:54 +0530

    Final Touches

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,25 +1,21 @@
-import React , {Fragment} from 'react';
+import { Fragment } from 'react';
+
 import HeaderCartButton from './HeaderCartButton';
 import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
-const Header = props =>{
-    return (
-        <Fragment>
-            <header className={classes.header}>
-                <h1>
-                Apna Khana
-                </h1>
-                <HeaderCartButton onClick={props.onShowCart}/>
-                
-            </header>
-            <div className={classes['main-image']}>
-                <img src={mealsImage} alt="Table full of food"/>
-            </div>
 
-        </Fragment>
-
-    );
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Dolo Foods</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
 };
-
 
 export default Header;
--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,9 +1,8 @@
 import classes from './MealsSummary.module.css';
 
-
 const MealsSummary = () => {
   return (
-    <section className={classes.summary}> 
+    <section className={classes.summary}>
       <h2>Delicious Food, Delivered To You</h2>
       <p>
         Choose your favorite meal from our broad selection of available meals
@@ -16,4 +15,5 @@ const MealsSummary = () => {
     </section>
   );
 };
+
 export default MealsSummary;
